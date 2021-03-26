b4ca181568a203528358900e0834f7224e520b4a
Ashish <ashrahurkar@yahoo.in>
2021-03-26 18:20:56 +0530

    Basic Testing support in Spring Framework

---
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -0,0 +1,67 @@
+package org.springframework.samples.petclinic.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.model.Vets;
+import org.springframework.samples.petclinic.service.ClinicService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+
+    @Mock
+    private ClinicService clinicService;
+
+    @InjectMocks
+    private VetController vetController;
+
+    private List<Vet> vetsList = new ArrayList<>();
+
+    @Mock
+    private Map<String, Object> model;
+
+    @BeforeEach
+    void setUp() {
+        vetsList.add(new Vet());
+
+        given(clinicService.findVets()).willReturn(vetsList);
+    }
+
+    @Test
+    void showVetList() {
+        // when
+        String viewName = vetController.showVetList(model);
+
+        // then
+        then(clinicService.findVets());
+        then(model.put(anyString(), any()));
+        assertThat("vets/vetList").isEqualToIgnoringCase(viewName);
+    }
+
+    @Test
+    void showResourcesVetList() {
+        // when
+        Vets returnedVets = vetController.showResourcesVetList();
+
+        // then
+        then(clinicService).should().findVets();
+        assertThat(returnedVets).isNotNull();
+    }
+}
